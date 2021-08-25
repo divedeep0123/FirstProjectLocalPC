2733922ad53792156333d9b7d8b4a6db0d231379
divedeep0123 <divedeep0123@gmail.com>
2021-08-25 10:11:18 -0700

    Second commit to feature

--- Hello.py
+++ Hello.py
@@ -3,6 +3,7 @@ from selenium import webdriver
 driver=webdriver.Chrome(executable_path="C:\\Users\\Deepak\\PycharmProjects\\FirstProject\\chromedriver.exe")
 driver.get("https://www.google.com")
 print("google launched")
+print("Google Successfully automated")
 driver.close()
 
 
